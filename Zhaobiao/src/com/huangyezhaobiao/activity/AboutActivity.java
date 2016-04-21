55171a5b6a64e5002410566b65c53bf94a30129c
shenzhixin <shenzhixin@58.com>
2016-04-21 15:54:48 +0800

    aaa

--- Zhaobiao/src/com/huangyezhaobiao/activity/AboutActivity.java
+++ Zhaobiao/src/com/huangyezhaobiao/activity/AboutActivity.java
@@ -16,7 +16,7 @@ import com.huangyezhaobiao.utils.VersionUtils;
  * 关于页面activity
  */
 public class AboutActivity extends QBBaseActivity implements OnClickListener {
-	private LinearLayout back_layout;
+	private LinearLayout back_layout;//haha
 	private TextView     txt_head,tv_version;
 	private String name;
 	private RelativeLayout rl_gongneng,rl_check_version;
