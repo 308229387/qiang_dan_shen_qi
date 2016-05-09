357e36fe7a46132ecee6e38aa9e2b199fa2af94c
chenguangming <chenguangming@58ganji.com>
2016-05-09 16:21:23 +0800

    自动登陆

--- Zhaobiao/src/com/huangyezhaobiao/activity/CommonFragmentActivity.java
+++ Zhaobiao/src/com/huangyezhaobiao/activity/CommonFragmentActivity.java
@@ -31,28 +31,30 @@ import com.xiaomi.mipush.sdk.MiPushClient;
 /**
  * Created by 58 on 2016/2/24.
  */
-public class CommonFragmentActivity extends FragmentActivity implements NetWorkVMCallBack{
+public class CommonFragmentActivity extends FragmentActivity implements NetWorkVMCallBack {
     private BackToForeVM backToForeVM;
     protected GlobalConfigVM globalConfigVM;
-
+    private static final String TAG = CommonFragmentActivity.class.getName();
     /**
      * 需要同步
+     *
      * @return
      */
-    private boolean needAsync(){
+    private boolean needAsync() {
         //当前时间戳
-        long currentTimeLine  = System.currentTimeMillis();
+        long currentTimeLine = System.currentTimeMillis();
         //从sp取时间戳
-        long latTimeLine     = 0;
+        long latTimeLine = 0;
         try {
-            latTimeLine = Long.valueOf(SPUtils.getVByK(CommonFragmentActivity.this,SPUtils.KEY_TIMELINE_GLOBAL));
+            latTimeLine = Long.valueOf(SPUtils.getVByK(CommonFragmentActivity.this, SPUtils.KEY_TIMELINE_GLOBAL));
         } catch (NumberFormatException e) {
-            latTimeLine  = 0;
+            latTimeLine = 0;
             e.printStackTrace();
         }
-        return TimeUtils.beyond24Hour(currentTimeLine,latTimeLine);//没有在时间戳范围内
+        return TimeUtils.beyond24Hour(currentTimeLine, latTimeLine);//没有在时间戳范围内
 
     }
+
     private NetWorkVMCallBack globalConfigCallBack = new NetWorkVMCallBack() {
         @Override
         public void onLoadingStart() {
@@ -62,27 +64,27 @@ public class CommonFragmentActivity extends FragmentActivity implements NetWorkV
         @Override
         public void onLoadingSuccess(Object t) {
             //进行存储操作
-            if(t instanceof GlobalConfigBean){
+            if (t instanceof GlobalConfigBean) {
                 GlobalConfigBean globalConfigBean = (GlobalConfigBean) t;
-                Log.e("shenzhixinww","global:"+globalConfigBean.toString());
+                Log.e("shenzhixinww", "global:" + globalConfigBean.toString());
                 //首先判断是不是需要增量拉取信息，存到SP中
                 String isIncrementalPull = globalConfigBean.getIsIncrementalPull();
-                SPUtils.saveKV(CommonFragmentActivity.this,GlobalConfigBean.KEY_isIncrementalPull,isIncrementalPull);
+                SPUtils.saveKV(CommonFragmentActivity.this, GlobalConfigBean.KEY_isIncrementalPull, isIncrementalPull);
                 //用户手机号
                 String status = globalConfigBean.getUserPhoneResult().getStatus();
-                if(TextUtils.equals(UserPhoneBean.SUCCESS,status)){
+                if (TextUtils.equals(UserPhoneBean.SUCCESS, status)) {
                     String userPhone = globalConfigBean.getUserPhoneResult().getUserPhone();
-                    SPUtils.saveKV(CommonFragmentActivity.this,GlobalConfigBean.KEY_USERPHONE,userPhone);
+                    SPUtils.saveKV(CommonFragmentActivity.this, GlobalConfigBean.KEY_USERPHONE, userPhone);
                 }
                 //网灵通信息
                 AccountExpireBean wltAlertResult = globalConfigBean.getWltAlertResult();
                 String expireState = wltAlertResult.getExpireState();
                 String msg = wltAlertResult.getMsg();
-                SPUtils.saveKV(CommonFragmentActivity.this,GlobalConfigBean.KEY_WLT_EXPIRE,expireState);
-                SPUtils.saveKV(CommonFragmentActivity.this,GlobalConfigBean.KEY_WLT_EXPIRE_MSG,msg);
-                Log.e("ashen","current:"+System.currentTimeMillis()+",userId:"+UserUtils.getUserId(CommonFragmentActivity.this));
+                SPUtils.saveKV(CommonFragmentActivity.this, GlobalConfigBean.KEY_WLT_EXPIRE, expireState);
+                SPUtils.saveKV(CommonFragmentActivity.this, GlobalConfigBean.KEY_WLT_EXPIRE_MSG, msg);
+                Log.e("ashen", "current:" + System.currentTimeMillis() + ",userId:" + UserUtils.getUserId(CommonFragmentActivity.this));
                 //更新时间戳
-                SPUtils.saveKV(CommonFragmentActivity.this,SPUtils.KEY_TIMELINE_GLOBAL,System.currentTimeMillis()+"");
+                SPUtils.saveKV(CommonFragmentActivity.this, SPUtils.KEY_TIMELINE_GLOBAL, System.currentTimeMillis() + "");
             }
         }
 
@@ -120,35 +122,41 @@ public class CommonFragmentActivity extends FragmentActivity implements NetWorkV
     }
 
     private void initVM() {
-        backToForeVM = new BackToForeVM(null,this);
+        backToForeVM = new BackToForeVM(null, this);
 
-        globalConfigVM = new GlobalConfigVM(globalConfigCallBack,this);
+        globalConfigVM = new GlobalConfigVM(globalConfigCallBack, this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if(SPUtils.fromBackground(this)){
+        if (SPUtils.fromBackground(this)) {
             //从后台进来的,加接口
             Log.e("shenzhiixn", "fromBackground");
             SPUtils.toForeground(this);//现在应用从后台到前台了
             backToForeVM.report();
-            if(needAsync() && !TextUtils.isEmpty(UserUtils.getUserId(this))){
+            if (needAsync() && !TextUtils.isEmpty(UserUtils.getUserId(this))) {
                 globalConfigVM.refreshUsers();
             }
-            dialog = new ZhaoBiaoDialog(this, "提示", "登录失败，您输入的账户名和密码不符!");
-            dialog.setCancelButtonGone();
-            dialog.setOnDialogClickListener(dialogClickListener);
-            loginViewModel = new LoginViewModel(vmCallBack,this);
-            Log.v("从后台进来的",UserUtils.getAccountName(this) + "===" +UserUtils.getAccountEncrypt(this));
-            loginViewModel.login(UserUtils.getAccountName(this), UserUtils.getAccountEncrypt(this),true);
-        }else{
-            Log.e("shenzhiixn","not fromBackground");
+            Log.v(TAG, "=>" + UserUtils.getAccountName(this));
+            Log.v(TAG, ">>" + UserUtils.getAccountEncrypt(this));
+            if (!"".equals(UserUtils.getAccountName(this)) && !"".equals(UserUtils.getAccountEncrypt(this))) {
+                dialog = new ZhaoBiaoDialog(this, "提示", "登录失败，您输入的账户名和密码不符!");
+                dialog.setCancelButtonGone();
+                dialog.setOnDialogClickListener(dialogClickListener);
+                loginViewModel = new LoginViewModel(vmCallBack, this);
+                Log.v("从后台进来的", UserUtils.getAccountName(this) + "===" + UserUtils.getAccountEncrypt(this));
+                loginViewModel.login(UserUtils.getAccountName(this), UserUtils.getAccountEncrypt(this), true);
+            }
+        } else {
+            Log.e("shenzhiixn", "not fromBackground");
         }
         BDMob.getBdMobInstance().onResumeActivity(this);
     }
 
-    /** added by chenguangming start**/
+    /**
+     * added by chenguangming start
+     **/
     private LogoutViewModel lvm;
     private ZhaoBiaoDialog.onDialogClickListener dialogClickListener = new ZhaoBiaoDialog.onDialogClickListener() {
         @Override
@@ -175,7 +183,7 @@ public class CommonFragmentActivity extends FragmentActivity implements NetWorkV
 
     /**
      * created by chenguangming 自动登录
-     *  */
+     */
     private LoginViewModel loginViewModel;
     private ZhaoBiaoDialog dialog;
 
@@ -195,7 +203,7 @@ public class CommonFragmentActivity extends FragmentActivity implements NetWorkV
         @Override
         public void onLoadingError(String msg) {
             try {
-                if(dialog!=null && !TextUtils.isEmpty(msg)){
+                if (dialog != null && !TextUtils.isEmpty(msg)) {
                     dialog.setMessage(msg);
                     dialog.show();
                 }
@@ -211,12 +219,12 @@ public class CommonFragmentActivity extends FragmentActivity implements NetWorkV
 
         @Override
         public void onNoInterNetError() {
-            Toast.makeText(CommonFragmentActivity.this, getString(R.string.no_network),Toast.LENGTH_SHORT).show();
+            Toast.makeText(CommonFragmentActivity.this, getString(R.string.no_network), Toast.LENGTH_SHORT).show();
         }
 
         @Override
         public void onLoginInvalidate() {
-            Toast.makeText(CommonFragmentActivity.this, getString(R.string.login_login_invalidate),Toast.LENGTH_SHORT).show();
+            Toast.makeText(CommonFragmentActivity.this, getString(R.string.login_login_invalidate), Toast.LENGTH_SHORT).show();
         }
 
         @Override
@@ -224,24 +232,27 @@ public class CommonFragmentActivity extends FragmentActivity implements NetWorkV
 
         }
     };
-    /** added by chenguangming end */
+
+    /**
+     * added by chenguangming end
+     */
 
     @Override
     protected void onStop() {
         super.onStop();
-        if(!BiddingApplication.getBiddingApplication().isAppOnForeground()){//到后台了
-            Log.e("shenzhiixn","to background");
+        if (!BiddingApplication.getBiddingApplication().isAppOnForeground()) {//到后台了
+            Log.e("shenzhiixn", "to background");
             SPUtils.toBackground(this);
             //openAppExitService();
-        }else{
-            Log.e("shenzhiixn","not to background");
+        } else {
+            Log.e("shenzhiixn", "not to background");
         }
     }
 
     /**
      * 开启appexitService
      */
-    protected void openAppExitService(){
+    protected void openAppExitService() {
         Intent intent = new Intent(this, AppExitService.class);
         startService(intent);
     }
@@ -290,7 +301,7 @@ public class CommonFragmentActivity extends FragmentActivity implements NetWorkV
 
     @Override
     public void onVersionBack(String version) {
-        Log.e("shenyy","version:"+version);
+        Log.e("shenyy", "version:" + version);
 
     }
 }
