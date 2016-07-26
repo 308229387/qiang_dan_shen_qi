65aeccc5d44f64da53f472196c6288dc3ff20fed
wangjianlong <wangjianlong@58ganji.com>
2016-07-26 10:38:23 +0800

    子账号管理

---
+++ Zhaobiao/src/com/huangyezhaobiao/bean/ChildAccountBean.java
@@ -0,0 +1,35 @@
+package com.huangyezhaobiao.bean;
+
+/**
+ * Created by 58 on 2016/7/22.
+ */
+public class ChildAccountBean {
+
+    private String accountId;
+    private String accountName;
+    private String accountPhone;
+
+    public String getAccountPhone() {
+        return accountPhone;
+    }
+
+    public void setAccountPhone(String accountPhone) {
+        this.accountPhone = accountPhone;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+}
