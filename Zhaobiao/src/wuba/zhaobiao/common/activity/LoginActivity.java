8f96f9e24e7f53640607b787a4af82659dbd449a
mengzhiyuan <mengzhiyuan@58ganji.com>
2016-09-18 21:03:09 +0800

    1、修改主题色紫色为红色； 2、修改登录后切换页面状态栏为透明；

--- Zhaobiao/src/wuba/zhaobiao/common/activity/LoginActivity.java
+++ Zhaobiao/src/wuba/zhaobiao/common/activity/LoginActivity.java
@@ -1,6 +1,8 @@
 package wuba.zhaobiao.common.activity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.huangyezhaobiao.R;
 
@@ -17,6 +19,12 @@ public class LoginActivity extends BaseActivity<LoginModel> {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blank);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT) {
+            //透明状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //透明导航栏
+//            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
         removeOtherActivity();
         init();
     }
